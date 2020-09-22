11a2f6879e6c47f34df20d018a4a7d05c40237f7
Renan Beraldi <beraldi2019renan@gmail.com>
2020-09-22 15:42:10 -0300

    Program Ended

---
+++ Using_block/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Using_block
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\Windows\Temp\file1.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
